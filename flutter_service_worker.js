6765aa811b433391e640c664c55cc6e60469f11a
Suraj Shah <surajshah_2001@yahoo.in>
2023-08-17 22:35:20 -0700

    Resume updated

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,9 +42,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "775c7bc9278acf6c66c87287e8f8b0e7",
-"/": "775c7bc9278acf6c66c87287e8f8b0e7",
-"main.dart.js": "037372d3a97ca5790d0f0da08d627f38",
+"index.html": "063bad9d4fd72977d25953c6249fdd91",
+"/": "063bad9d4fd72977d25953c6249fdd91",
+"main.dart.js": "b06234800369f27aa4f4eaecb6e17f05",
 "manifest.json": "e74af8957b5899dc6da961caee768ec9",
 "version.json": "009c9e65172e010890f7f65fde438006"
 };
